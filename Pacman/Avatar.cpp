c2b666a6140c6ebd4bbd74fdfdbddaa8ab69e67b
Faizhal Bin Bakar <faizhalb@gmail.com>
2017-09-08 22:17:22 +0300

    added Avatar chomping animation

--- Pacman/Avatar.cpp
+++ Pacman/Avatar.cpp
@@ -2,12 +2,28 @@
 
 Avatar::Avatar(const Vector2f& aPosition)
 : MovableGameEntity(aPosition, "open_32.png")
+, mEnumAvatarFacing(AVATAR_FACING_RIGHT) //this corresponds to open_32.png 
 {
 
 }
 
 Avatar::~Avatar(void)
 {
+}
+
+void Avatar::Init()
+{
+	//the avatar will have textures set according to indices calculated in SetAnimation()
+	arrImage[0] = AssetManager::GetInstance()->GetImageAsset("open_32.png");
+	arrImage[1] = AssetManager::GetInstance()->GetImageAsset("closed_32.png");
+	arrImage[2] = AssetManager::GetInstance()->GetImageAsset("open_down_32.png");
+	arrImage[3] = AssetManager::GetInstance()->GetImageAsset("closed_down_32.png");
+	arrImage[4] = AssetManager::GetInstance()->GetImageAsset("open_left_32.png");
+	arrImage[5] = AssetManager::GetInstance()->GetImageAsset("closed_left_32.png");
+	arrImage[6] = AssetManager::GetInstance()->GetImageAsset("open_up_32.png");
+	arrImage[7] = AssetManager::GetInstance()->GetImageAsset("closed_up_32.png");
+
+	GameEntity::Init();
 }
 
 void Avatar::Update(float aTime)
@@ -18,6 +34,8 @@ void Avatar::Update(float aTime)
 	Vector2f direction = destination - myPosition;
 
 	float distanceToMove = aTime * 30.f;
+	
+	UpdateFacingDirection(direction);
 
 	if (distanceToMove > direction.Length())
 	{
@@ -29,5 +47,68 @@ void Avatar::Update(float aTime)
 	{
 		direction.Normalize();
 		myPosition += direction * distanceToMove;
+	}
+
+	SetAnimation();
+}
+
+void Avatar::UpdateFacingDirection(Vector2f vec)
+{
+	if (vec.myX == 0 && vec.myY == 0)
+	{
+		//do not change facing direction
+		return;
+	}
+
+	if (vec.myX > 0)
+	{
+		mEnumAvatarFacing = AVATAR_FACING_RIGHT;
+	}
+	else if (vec.myX < 0)
+	{
+		mEnumAvatarFacing = AVATAR_FACING_LEFT;
+	}
+	else if (vec.myY > 0)
+	{
+		mEnumAvatarFacing = AVATAR_FACING_DOWN;
+	}
+	else
+	{
+		mEnumAvatarFacing = AVATAR_FACING_UP;
+	}
+}
+
+void Avatar::SetAnimation()
+{
+	int subIndex = ShouldAvatarBite() ? 0 : 1;
+
+	int majorIndex = mEnumAvatarFacing;
+
+	myImageAssetCache = arrImage[majorIndex * 2 + subIndex];
+}
+
+bool Avatar::ShouldAvatarBite() const
+{
+	if (mEnumAvatarFacing == AVATAR_FACING_RIGHT || mEnumAvatarFacing == AVATAR_FACING_LEFT)
+	{
+		if ((int)myPosition.myX % 22 < 11)
+		{
+			return true; 
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		if ((int)myPosition.myY % 22 < 11)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
--- Pacman/Avatar.h
+++ Pacman/Avatar.h
@@ -3,6 +3,15 @@
 
 #include "MovableGameEntity.h"
 #include "Vector2f.h"
+#include "AssetManager.h"
+
+typedef enum
+{
+	AVATAR_FACING_RIGHT = 0,
+	AVATAR_FACING_DOWN,
+	AVATAR_FACING_LEFT,
+	AVATAR_FACING_UP
+} eAvatarFacing;
 
 class Avatar : public MovableGameEntity
 {
@@ -10,11 +19,22 @@ public:
 	Avatar(const Vector2f& aPosition);
 	~Avatar(void);
 
+	void Init() override;
+
 	void Update(float aTime);
 
 private:
+	//this determines if avatar should bite down depending on its position within a cell
+	bool ShouldAvatarBite() const;
+	 
+	//determinds which avatar asset to be used and assigns it to the active image to be drawn
+	void SetAnimation();
 
-
+	//takes in a direction vector of the avatars movement and stores where
+	//avatar is currently facing
+	void UpdateFacingDirection(Vector2f vec);
+	eAvatarFacing mEnumAvatarFacing;
+	ImageAssetCacheSPtr arrImageCache[8];
 };
 
 #endif //AVATAR_H
